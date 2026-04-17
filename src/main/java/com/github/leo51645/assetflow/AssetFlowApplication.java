b004ff17afe394ea2d603aadefecc0eef61bcf66
Leo51645 <fischer.leonard@gmx.de>
2026-04-17 22:27:59 +0200

    Initial commit

---
+++ src/main/java/com/github/leo51645/assetflow/AssetFlowApplication.java
@@ -0,0 +1,13 @@
+package com.github.leo51645.assetflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/leo51645/assetflow/AssetFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leo51645.assetflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
