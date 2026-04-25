6b874f832394d4d431538ddfebfd9c29a4315805
Leo51645 <fischer.leonard@gmx.de>
2026-04-26 00:43:02 +0200

    feat: add auth and logout functionality to the auth controller

--- src/main/java/com/github/leo51645/assetflow/security/controller/AuthController.java
+++ src/main/java/com/github/leo51645/assetflow/security/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.github.leo51645.assetflow.security.controller;
 
+import com.github.leo51645.assetflow.security.domain.dto.request.AuthRequestDto;
 import com.github.leo51645.assetflow.security.domain.dto.response.AuthResponseDto;
 import com.github.leo51645.assetflow.security.service.AuthService;
+import com.github.leo51645.assetflow.security.service.CookieService;
 import com.github.leo51645.assetflow.user.domain.dto.request.RegisterRequestDto;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,12 +22,52 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final CookieService cookieService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponseDto> register(@RequestBody @Valid RegisterRequestDto request, HttpServletResponse response) {
         AuthResponseDto responseDto = authService.register(request);
 
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthResponseDto> authenticate(
+            @Valid @RequestBody AuthRequestDto request,
+            HttpServletResponse response
+    ) {
+        AuthResponseDto responseDto = authService.authenticate(request);
+        cookieService.addRefreshTokenCookie(response, responseDto.getRefreshToken());
+
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<AuthResponseDto> refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        String refreshToken = cookieService.extractRefreshTokenFromCookie(request);
+        if (refreshToken == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        AuthResponseDto responseDto = authService.refreshToken(refreshToken);
+        cookieService.addRefreshTokenCookie(response, responseDto.getRefreshToken());
+
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        String refreshToken = cookieService.extractRefreshTokenFromCookie(request);
+        authService.logout(refreshToken);
+        cookieService.clearRefreshTokenCookie(response);
+
+        return ResponseEntity.ok().build();
     }
 
 }
