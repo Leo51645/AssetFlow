f91613b3d1bb6778ce9a0765787c846dcb32336d
Leo51645 <fischer.leonard@gmx.de>
2026-05-03 00:03:28 +0200

    fix: wrong object type at createdAt in the UserResponseDto

--- src/main/java/com/github/leo51645/assetflow/user/domain/dto/response/UserResponseDto.java
+++ src/main/java/com/github/leo51645/assetflow/user/domain/dto/response/UserResponseDto.java
@@ -3,9 +3,10 @@ package com.github.leo51645.assetflow.user.domain.dto.response;
 import com.github.leo51645.assetflow.security.domain.entity.Role;
 import lombok.Builder;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Builder
-public record UserResponseDto(Long id, String email, String firstName, String lastName, LocalDate birthday, LocalDateTime createdAt, Role role) {
+public record UserResponseDto(Long id, String email, String firstName, String lastName, LocalDateTime birthday, Instant createdAt, Role role) {
 }
