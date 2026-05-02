06aa9b279c62e99d58382f876a368f423e67592f
Leonard Fischer <fischer.leonard@gmx.de>
2026-05-03 00:39:07 +0200

    fix: startup & compiling bugs (#5)
    * fix: missing final statement in UserController
    
    * fix: wrong object type at createdAt in the UserResponseDto
    
    * fix: two h2 database dependencies in pom.xml
    
    * fix: wrong object type in UserResponseDto at variable birthday
    
    * fix: couldn't build properly due to wrong secret-key

--- src/main/java/com/github/leo51645/assetflow/user/controller/UserController.java
+++ src/main/java/com/github/leo51645/assetflow/user/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    private UserDtoMapper userDtoMapper;
+    private final UserDtoMapper userDtoMapper;
 
     @GetMapping("/me")
     public ResponseEntity<UserResponseDto> getCurrentUser(@AuthenticationPrincipal UserEntity userEntity) {
