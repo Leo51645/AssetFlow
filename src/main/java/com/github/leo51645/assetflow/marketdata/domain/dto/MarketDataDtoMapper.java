a289ea227a2e74931e02a434515da5f3d05de3c0
Leo51645 <197633502+Leo51645@users.noreply.github.com>
2026-08-24 22:17:56 +0200

    refactor: move http response parse method to nearest service

--- src/main/java/com/github/leo51645/assetflow/marketdata/domain/dto/MarketDataDtoMapper.java
+++ src/main/java/com/github/leo51645/assetflow/marketdata/domain/dto/MarketDataDtoMapper.java
@@ -1,23 +1,21 @@
 package com.github.leo51645.assetflow.marketdata.domain.dto;
 
 import com.github.leo51645.assetflow.investment_asset.domain.entity.InvestAssetEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class MarketDataDtoMapper {
 
-    public MarketDataResponseDto toMarketDataDto(String raw) {
-        return null;
-    }
-
-    public InvestAssetEntity toInvestAssetEntity(MarketDataResponseDto marketDataResponseDto) {
+    public InvestAssetEntity toInvestAssetEntity(MarketDataFigiResponseDto figiResponseDto, MarketDataYahooResponseDto yahooResponseDto) {
         return InvestAssetEntity.builder()
                 .id(null)
-                .name(marketDataResponseDto.getName())
-                .isin(marketDataResponseDto.getIsin())
-                .symbol(marketDataResponseDto.getSymbol())
-                .currency(marketDataResponseDto.getCurrency())
-                .assetType(marketDataResponseDto.getAssetType())
+                .name(figiResponseDto.getName())
+                .isin(figiResponseDto.getIsin())
+                .symbol(figiResponseDto.getSymbol())
+                .currency(yahooResponseDto.getCurrency())
+                .assetType(figiResponseDto.getAssetType())
                 .build();
     }
 }
