4f3dc8d8631381a21912f8994e684760603c0158
Leo51645 <fischer.leonard@gmx.de>
2026-08-24 10:57:39 +0200

    feat: add marketdata dto & mapper

--- src/main/java/com/github/leo51645/assetflow/marketdata/domain/dto/MarketDataResponseDto.java
+++ src/main/java/com/github/leo51645/assetflow/marketdata/domain/dto/MarketDataResponseDto.java
@@ -1,12 +1,17 @@
 package com.github.leo51645.assetflow.marketdata.domain.dto;
 
+import com.github.leo51645.assetflow.investment_asset.domain.entity.AssetType;
+import com.github.leo51645.assetflow.investment_asset.domain.entity.Currency;
 import lombok.*;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class MarketDataDto {
+public class MarketDataResponseDto {
+    private String name;
     private String isin;
-    private 
+    private String symbol;
+    private Currency currency;
+    private AssetType assetType;
 }
