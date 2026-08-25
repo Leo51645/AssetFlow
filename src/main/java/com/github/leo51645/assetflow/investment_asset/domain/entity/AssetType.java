fb3ed78acfd22b2b8909258e047384359616710b
Leo51645 <197633502+Leo51645@users.noreply.github.com>
2026-08-25 13:16:53 +0200

    remove: bond category in asset type enum & change empty to other

--- src/main/java/com/github/leo51645/assetflow/investment_asset/domain/entity/AssetType.java
+++ src/main/java/com/github/leo51645/assetflow/investment_asset/domain/entity/AssetType.java
@@ -3,8 +3,7 @@ package com.github.leo51645.assetflow.investment_asset.domain.entity;
 public enum AssetType {
     STOCK,
     ETF,
-    BOND,
     CRYPTO,
     COMMODITY, // Gold, Oil, etc.
-    EMPTY;
+    OTHER;
 }
