d4df6f5347fee916c7a6f38a8db6fe8be56c2f6d
Leo51645 <197633502+Leo51645@users.noreply.github.com>
2026-08-26 11:45:05 +0200

    feat: add helper methods for yahoo chart service

--- src/main/java/com/github/leo51645/assetflow/marketdata/util/MarketDataUtil.java
+++ src/main/java/com/github/leo51645/assetflow/marketdata/util/MarketDataUtil.java
@@ -2,6 +2,7 @@ package com.github.leo51645.assetflow.marketdata.util;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.leo51645.assetflow.investment_asset.domain.entity.AssetType;
+import com.github.leo51645.assetflow.investment_asset.domain.entity.Currency;
 import com.github.leo51645.assetflow.marketdata.domain.dto.MarketDataYahooSearchResponseDto;
 import org.springframework.stereotype.Component;
 
@@ -20,8 +21,30 @@ public class MarketDataUtil {
         return assetType;
     }
 
+    public Currency getCurrency(String currencyString) {
+        Currency currency = Currency.USD;
+
+        if (currencyString == null || currencyString.isEmpty()) {
+            return currency;
+        }
+
+        switch (currencyString) {
+            case "EUR" -> currency = Currency.EUR;
+            case "GBP" -> currency = Currency.GBP;
+            case "CHF" -> currency = Currency.CHF;
+            case "JPY" -> currency = Currency.JPY;
+            case "CNY" -> currency = Currency.CNY;
+        }
+
+        return currency;
+    }
+
     public String createYahooFinanceSearchRequestURI(String requestParam) {
         return "https://query1.finance.yahoo.com/v1/finance/search?q=" + requestParam;
+    }
+
+    public String createYahooFinanceChartRequestURI(String symbol) {
+        return "https://query1.finance.yahoo.com/v8/finance/chart/" + symbol + "?range=1d&interval=1d";
     }
 
     public boolean isIsin(String requestParam) {
