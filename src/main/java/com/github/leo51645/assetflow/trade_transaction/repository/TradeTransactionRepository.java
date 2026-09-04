2c9e60caea9e3c17a4fb002c68518e8cb2d8f1f0
Leo51645 <197633502+Leo51645@users.noreply.github.com>
2026-09-04 18:24:05 +0200

    feat: add delete funcitonality to trade transaction repository

--- src/main/java/com/github/leo51645/assetflow/trade_transaction/repository/TradeTransactionRepository.java
+++ src/main/java/com/github/leo51645/assetflow/trade_transaction/repository/TradeTransactionRepository.java
@@ -2,10 +2,15 @@ package com.github.leo51645.assetflow.trade_transaction.repository;
 
 import com.github.leo51645.assetflow.trade_transaction.domain.entity.TradeTransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TradeTransactionRepository extends JpaRepository<TradeTransactionEntity, Long> {
     List<TradeTransactionEntity> findAllByUser_Id(Long userId);
     List<TradeTransactionEntity> findAllByInvestAsset_Id(Long investAssetId);
+
+    void deleteAllByUser_Id(Long userId);
+    void deleteAllByInvestAsset_Id(Long investAssetId);
 }
