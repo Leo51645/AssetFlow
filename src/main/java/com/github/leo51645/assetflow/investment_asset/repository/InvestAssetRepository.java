31ec3eee6dc3a96af4b095a8e8d7b08900174887
Leo51645 <197633502+Leo51645@users.noreply.github.com>
2026-09-07 21:36:33 +0200

    feat: find outdated assets with last update greater or equals 10 min in InvestAssetEntity DB

--- src/main/java/com/github/leo51645/assetflow/investment_asset/repository/InvestAssetRepository.java
+++ src/main/java/com/github/leo51645/assetflow/investment_asset/repository/InvestAssetRepository.java
@@ -2,8 +2,12 @@ package com.github.leo51645.assetflow.investment_asset.repository;
 
 import com.github.leo51645.assetflow.investment_asset.domain.entity.InvestAssetEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +16,7 @@ public interface InvestAssetRepository extends JpaRepository<InvestAssetEntity, 
     Optional<InvestAssetEntity> findBySymbol(String symbol);
 
     boolean existsBySymbol(String symbol);
+
+    @Query("SELECT a FROM InvestAssetEntity a where a.priceUpdatedAt <= :updatedBefore")
+    List<InvestAssetEntity> findOutdatedAssets(@Param("updatedBefore") LocalDateTime updatedBefore);
 }
