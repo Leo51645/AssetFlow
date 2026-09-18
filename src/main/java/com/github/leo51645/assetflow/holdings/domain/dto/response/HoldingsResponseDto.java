7bd0bf4a58a19d781cf00431ac3e12c80858f4a4
Leo51645 <197633502+Leo51645@users.noreply.github.com>
2026-09-18 20:04:43 +0200

    fix: assetData in HoldingsResponseDto needs to be a list

--- src/main/java/com/github/leo51645/assetflow/holdings/domain/dto/response/HoldingsResponseDto.java
+++ src/main/java/com/github/leo51645/assetflow/holdings/domain/dto/response/HoldingsResponseDto.java
@@ -3,8 +3,9 @@ package com.github.leo51645.assetflow.holdings.domain.dto.response;
 import lombok.Builder;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Builder
-public record HoldingsResponseDto(String email, AssetData assetData) {
+public record HoldingsResponseDto(String email, List<AssetData> assetData) {
     public record AssetData(String assetName, String assetSymbol, long quantity, BigDecimal avgBuyPrice) {}
 }
